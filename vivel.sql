bc5089dc8b6c29dc9addf313bb6b5f21cef843cc
GeekBran <131333150+GeekBran@users.noreply.github.com>
2023-06-05 09:23:38 -0600

    Add files via upload

--- vivel.sql
+++ vivel.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 23-05-2023 a las 04:43:10
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.1.2
+-- Host: 127.0.0.1
+-- Generation Time: Jun 04, 2023 at 08:06 PM
+-- Server version: 11.1.0-MariaDB
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,44 +18,88 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `vivel`
+-- Database: `vivel`
 --
-CREATE DATABASE IF NOT EXISTS `vivel` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `vivel`;
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `contacto`
+-- Table structure for table `articulos`
 --
 
-CREATE TABLE `contacto` (
-  `id` int(50) NOT NULL,
-  `correo` varchar(35) NOT NULL,
-  `nombre` varchar(35) NOT NULL,
-  `numero` int(10) NOT NULL,
-  `pregunta` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE IF NOT EXISTS `articulos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Titulo` varchar(100) NOT NULL,
+  `Imagen` varchar(100) DEFAULT NULL,
+  `Texto` text NOT NULL,
+  `Autor` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Índices para tablas volcadas
+-- Dumping data for table `articulos`
 --
 
---
--- Indices de la tabla `contacto`
---
-ALTER TABLE `contacto`
-  ADD PRIMARY KEY (`id`);
+INSERT INTO `articulos` (`id`, `Titulo`, `Imagen`, `Texto`, `Autor`) VALUES
+(1, 'El dolar al día de hoy', 'imagenes/DOLAR.jpg', '¿Sabías que el dolar alcanzo su nivel más bajo desde hace 30 años?', 'Gibran Palma');
+
+-- --------------------------------------------------------
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- Table structure for table `clientes`
 --
 
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `Clave` int(11) NOT NULL AUTO_INCREMENT,
+  `Correo` varchar(40) NOT NULL,
+  `Nombre` varchar(40) NOT NULL,
+  `Numero` varchar(12) NOT NULL,
+  `Pregunta` varchar(100) NOT NULL,
+  PRIMARY KEY (`Clave`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
 --
--- AUTO_INCREMENT de la tabla `contacto`
+-- Table structure for table `cursos`
 --
-ALTER TABLE `contacto`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+CREATE TABLE IF NOT EXISTS `cursos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Titulo` varchar(100) NOT NULL,
+  `Imagen` varchar(100) NOT NULL,
+  `Texto` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cursos`
+--
+
+INSERT INTO `cursos` (`id`, `Titulo`, `Imagen`, `Texto`) VALUES
+(1, 'CREACIÓN DE EMPRESAS', 'imagenes/empresa.jpg', 'Si de verdad quieres dar el paso para emprender, este es el programa ideal para ti. Te llevaremos de la mano y a través de 12 sesiones impartidas por mentores expertos que te ayudarán a construir de 0 a 100 tu modelo de negocio.'),
+(2, 'MARKETING PARA EMPRENDEDORES', 'imagenes/MARKETING.jpg', '¿Eres emprendedor y te cuesta llegar a más clientes para aumentar tus ventas? ¿Haces inversiones en publicidad, pero no obtienes los resultados que buscas? Sin duda este programa te ayudará a comunicar y conectar mejor con tus clientes potenciales, generar el interés y ventas para que tu inversión en marketing sea más efectiva.'),
+(3, 'FINANZAS PARA EMPRENDEDORES', 'imagenes/mobile-phone-5836879_1280.png', 'Todo emprendedor exitoso debe llevar las finanzas de su empresa en orden, es por ello que este programa está diseñado para que cualquier persona, aún sin ser especialista en finanzas, pueda comprender las finanzas de su negocio y lo más importante, ponerlas en orden para tener una empresa sana financieramente.'),
+(4, 'ASESORÍAS PERSONALIZADA PARA EMPRESAS', 'imagenes/asesor-1-e1562661405109.jpg', 'Si tienes una empresa en funcionamiento y deseas dar el paso en su siguiente nivel de desarrollo, las asesorías especializadas y orientadas a tus necesidades, son la mejor opción para que puedas resolver los obstáculos actuales y generar crecimiento en tu industria.'),
+(5, 'MENTORING DIRECTIVO', 'imagenes/mentoring-beneficios-1200x675.jpg', 'Toda organización requiere de tomadores de decisiones capaces y preparados para afrontar los retos que demanda el mundo actual. Este programa de Mentoring Directivo está hecho especialmente para acompañar y desarrollar a los líderes organizacionales a través de sesiones de aprendizaje significativo enfocadas en ayudarles a mejorar aspectos muy específicos que los hagan seguir su camino de crecimiento.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `usuario` varchar(40) NOT NULL,
+  `contraseña` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usuario`
+--
+
+INSERT INTO `usuario` (`usuario`, `contraseña`) VALUES
+('ADMIN', '12345');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
