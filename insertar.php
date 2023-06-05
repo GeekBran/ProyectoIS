fae53a55b8800dd8eaf65450a69af52b2f2bf93e
GeekBran <131333150+GeekBran@users.noreply.github.com>
2023-06-04 21:28:45 -0600

    Add files via upload

--- insertar.php
+++ insertar.php
@@ -1,31 +1,30 @@
 <?php
-include("conexion.php");
-$getmysql=new mysqlconex();
-$getconex=getmysql->conex();
+$mysqli =new mysqli("localhost","root","","vivel");
 
-if(isset($_POST["Enviar"])){
-
-    $correo=$_POST["Correo"];
-    $nombre=$_POST["Nombre"];
-    $numero=$_POST["Numero"];
-    $pregunta=$_POST["Pregunta"];
-    
-    $query="INSERT INTO contacto(correo,nombre, numero, pregunta) VALUES(?,?,?,?)";
-    $sentencia = mysqli_prepare($getconex,$query);
-    mysqli_stmt_bind_param($sentenca, "SSIS",$correo,$nombre,$numero,$pregunta);
-    mysqli_stmt_execute($sentenca);
-    $afetado=mysqli_stmt_affected_rows($sentenca);
-    if($afectado==1){
-        echo "<script> alert('Sus dudas han sido enviadas'); location.href='../Index.php'; </script>";
-
-
-    }else{
-            echo "<script> alert('lo sentimos no se pudo enviar sus dudas'); location.href='../Index.php'; </script>";
-
-        }
-    mysqli_stmt_close(sentencia);
-    mysql_close($getconex);
-
+if($mysqli->connect_errno){
+    echo "Lo sentimo este sitio web esta experimentando problemas";
+    exit;
 }
+//rec var
+if(isset($_POST['Correo'],$_POST['Nombre'],$_POST['Numero'],$_POST['Pregunta'])
+and $_POST['Correo'] !="" and $_POST['Nombre'] !="" and $_POST['Numero'] !="" and $_POST['Pregunta'] !=""){
+
+    $correo = $_POST["Correo"];
+    $nombre = $_POST["Nombre"];
+    $numero = $_POST["Numero"];
+    $pregunta = $_POST["Pregunta"];
+
+    $consulta = "INSERT INTO contacto (correo,nombre,numero,pregunta)VALUES ('$correo', '$nombre', '$numero', '$pregunta')";
+
+        if(mysqli_query($mysqli,$consulta)){
+            echo "<script> alert('Sus dudas han sido enviadas.'); location.href='Index.html' </script>"; 
+        }else{
+            echo"<script> alert('Fallo al enviar sus dudas.'); location.href='Index.html' </script>";
+        }
+}
+else{
+    echo"<script> alert('Fallo al enviar sus dudas.'); location.href='Index.html' </script>";
+}
+
 
 ?>
