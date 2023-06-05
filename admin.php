08057028f23ea848a4489f0aedad2f1df4d49318
GeekBran <131333150+GeekBran@users.noreply.github.com>
2023-06-04 21:27:12 -0600

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="es">
+  
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-2Nj6voGZriNpCfzS1wLpUWe+I1q3X/q6h5G5oZv6oNSHmIh1dpvcJLEwMv5yt6hv" crossorigin="anonymous">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link href="imagenes/Icono.png" type="image/x-icon" rel="shortcut icon" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Overpass&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="admin.css">
+
+  <title>VIVEL</title>
+</head>
+
+<body>
+<header role="banner">
+<nav role="navigation" class="nav">
+          <div class="nav-content">
+            <div class="nav-content-logo"><a class="Logitoo" href="index.php"></a></div>
+            <div class="nav-content-title">
+            </div>
+            <div class="nav-content-btns">
+              <nav class="nav-btns">
+                <div class="nav-content-btn-1"><a href="courses.html">Cursos</a></div>
+                <div class="nav-content-btn-2"><a href="#arti">Noticias</a></div>
+                <div class="nav-content-btn-3"><a href="#Ases">Asesores</a></div>
+                <div class="nav-content-btn-4"><a href="#contact">Contacto</a></div>
+                <div class="nav-content-btn-5"><a href="SHOP.html">Obtener  Curso</a></div>
+                <div class="nav-content-btn-6"><a href="login.php">Login</a></div>
+              </nav>
+            </div>
+          </div>
+        </nav>
+</header>
+
+<div class="header2">
+    <div col-md-10>
+      <h1>Administracion Panel de Control</h1>
+    
+    </div>
+    <div col-md-2>
+    <div class="dropdown">
+  <button class="des">Crear Contenido</button>
+  <div class="dropdown-content">
+  <a href="https://blog.hubspot.com/">Agregar video promosional</a>
+  <a href="https://academy.hubspot.com/">Agregar Asesores</a>
+  <a href="https://www.youtube.com/user/hubspot">Agregar Cursos</a>
+  <a href="https://www.youtube.com/user/hubspot">Agregar Articulos</a>
+  </div>
+</div>
+</div>
+
+
+
+
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-Q/S+dJh2uVXzccyohmLZqn/Ztzf3hBcq7zFj33oysq+8xyv5jNk5ScP9JFr6QD8Z" crossorigin="anonymous"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+</body>
+
+
+</html>
